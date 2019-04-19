4308439a28dabb9b880cdb6dcbc983dbe2f24c3c
vanbommel8 <vanbommel8@gmail.com>
2019-04-19 11:12:26 +0200

    add create users api

--- create/create.js
+++ create/create.js
@@ -1,17 +1,27 @@
-var User = require('mongoose');
 
-// create a new user
-var newUser = mongoose.Schema({
-  name: Peter,
-  surname: Quill,
-  email: starlord55@avengers.com,
-  dateOfBirth: 32,
-  gender: male
+
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var posts = require('./routes/users');
+app.use('/users', users);
+
+
+
+var newUser = new User({ name: 'Thanos' });
+newUser.save(function (err) {
+if (err) return handleError(err);
+// saved!
+})
+
+
+app.set('port', (process.env.PORT || 3001));
+app.listen(app.get('port'), function () {
+    console.log('Server has started! http://localhost:' + app.get('port') + '/');
 });
-
-// save the user
-newUser.save(function(err) {
-  if (err) throw err;
-
-  console.log('User created!');
-});
+module.exports = app;
