6225d6d6763ec699557bc689d37ab43296956920
vanbommel8 <vanbommel8@gmail.com>
2019-04-20 17:19:41 +0200

    use morgan npm for logging request details

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
-var User = require('mongoose');
-const express = require('express');
-var app=express();
-const User = require('../models/model');
+var express = require ('express');
+var app = express();
+var cors = require('cors')
+var read = require ("./routes/read.js");
+var modify = require ("./routes/modify.js");
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var path = require('path');
+const host = 'localhost';
+const dbName = 'myDatabase';
+const mongoose = require('mongoose');
 
-// create a new user
-app.post('/',function(req,res){ 
-  var newUser = new User(req.body);
-  newUser.save(function (err) {
-  if (err) return handleError(err);
-  // saved!
-  });})
-  
 
+//connection to database
+mongoose.connect('mongodb://'+ host + '/' + dbName);
+var db = mongoose.connection;
+db.on('error', function() {
+console.error('Connection error!')
+});
+db.once('open', function() {
+console.log('DB connection Ready');
+});
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/", read);
+app.use("/modify", modify);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Setup logger and body parser
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// Setup static public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.listen(3000);
+
+module.exports = app;
