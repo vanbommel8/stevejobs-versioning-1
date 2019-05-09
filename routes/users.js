d0a67329f943fd73a02080e52f54eb3ea3a6261d
Angeek94 <CorsaroAngelo1994@hotmail.it>
2019-05-09 16:51:23 +0200

    final develop

--- routes/users.js
+++ routes/users.js
@@ -26,6 +26,13 @@ router.post('/',
       res.json(user);
     });
   });
+  router.get('/:name', function(req, res) {
+    User.findOne({_id: req.params.name}, function(err, user) {
+      if (err) return res.status(500).json({error: err});
+      if (!user) return res.status(404).json({message: 'Utente non trovato'});
+      res.json(user);
+    });
+  });
 
   router.put('/:id', function(req, res) {
     User.findOne({_id: req.params.id})
@@ -45,6 +52,7 @@ router.post('/',
           });
         });
   });
+  
   router.delete('/:id', function(req, res) {
     User.findOne({_id: req.params.id})
         .exec(function(err, user) {
