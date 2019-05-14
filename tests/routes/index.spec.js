7d0175084ac614a83459c5ad5b3d320b22ed47e9
Angeek94 <CorsaroAngelo1994@hotmail.it>
2019-05-14 17:21:20 +0200

    modify test

--- tests/routes/index.spec.js
+++ tests/routes/index.spec.js
@@ -5,14 +5,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
-describe('GET /', () => {
-    const expectedResponse = {message: 'API Works'};
-    it('Test index API payload', async () => {
-      const result = await chai.request(app).get('/');
-      expect(result.body).to.be.deep.equal(expectedResponse);
-      expect(result.status).to.be.equal(200);
-    });
-  });
+
   describe('GET /', () => {
     const expectedResponse = {message: 'API Works'};
     it('Test index API payload', async () => {
