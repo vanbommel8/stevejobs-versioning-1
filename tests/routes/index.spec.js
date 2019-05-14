84dfaa0746b98ca32c8d3a4ad31fa42990412dcf
Daniele <vanbommel8@gmail.com>
2019-05-14 20:54:35 +0200

    fix test

--- tests/routes/index.spec.js
+++ tests/routes/index.spec.js
@@ -6,8 +6,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
-
-  describe('GET /', () => {
+describe('GET /', () => {
     const expectedResponse = {message: 'API Works'};
     it('Test index API payload', async () => {
       const result = await chai.request(app).get('/');
@@ -15,7 +14,65 @@ chai.use(chaiHttp);
       expect(result.status).to.be.equal(200);
     });
   });
- 
+
+
+  describe('POST /', function () {
+    it('Dovrebbe aggiungere un singolo user', function (done) {
+    
+        let newUser = {
+            name: 'Thanos',
+            surname: 'Rossi',
+            email: 'thanos@gmail.com',
+            dateOfBirth: '24-08-1204',
+            gender: 'M'
+        };
+
+        chai.request(app)
+        .post('/users')
+        .send(newUser)
+        .end(function (err, res) {
+            expect(res.status).to.equal(201);
+            expect(res.body).to.be.a('object');
+            expect(res.body).to.have.a.property('name');
+            expect(res.body).to.have.a.property('surname');
+            expect(res.body).to.have.a.property('email');
+            expect(res.body).to.have.a.property('dateOfBirth');
+            expect(res.body).to.have.a.property('gender');
+            expect(res.body._id).to.exist;
+            done();
+        });
+});
+
+
+});
+
+
+
+
+describe('PUT /', function () {
+  it('Dovrebbe aggiornare un singolo User cercato per id: PUT', function (done) {
+        
+    let user = new User ({
+      name: 'Thanos',
+      surname: 'Rossi',
+      email: 'thanos@gmail.com',
+      dateOfBirth: '24-08-1204',
+      gender: 'M'
+    });
+
+    user.save(function (err, user) {
+        chai.request(app)
+        .put('/users/' + user.id)
+        .end(function (err, res) {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.be.a('object');
+            expect(res.body._id).to.equal(user.id);
+            done();
+        });
+    });
+});
+})
+
 
   describe('404 Route', () => {
     const expectedResponse = {message: 'Not Found', error: {status: 404}};
@@ -25,64 +82,3 @@ chai.use(chaiHttp);
       expect(result.status).to.be.equal(404);
     });
   });
-
-  it('test per cercare un utente tramite id: ', function (done) {
-    let user = new User ({
-        name: "Giuseppe",
-        surname: "Privitera",
-        email: "giuseppeprivitera@libero.it",
-        dateOfBirth: "10-05-2000",
-        gender: "Maschio"
-    });
-    user.save(function (err, user) {
-        chai.request(app)
-        .get('/users/' + user.id)
-        .end(function (err, res) {
-            expect(res.status).to.equal(200);
-            expect(res.body).to.be.a('object');
-            expect(res.body._id).to.equal(user.id);
-            done();
-        });
-    });
-});
-
-it("test per eliminare un utente ", function (done) {
-
-  const response = { message: 'Utente eliminato correttamente' };
-
-  let user = new User ({
-      name: "Giuseppe",
-      surname: "Privitera",
-      email: "giuseppe.privitera@gmail.com",
-      dateOfBirth: "27-10-2000",
-      gender: "Maschio"
-  });
-  user.save((err, user) => {
-      chai.request(app)
-      .del('/users/' + user.id)
-      .end((err, res) => {
-          expect(res.body).to.be.deep.equal(response);
-          expect(res.status).to.equal(200);
-          done();
-      });
-  });
-});
-it('test per cercare un utente tramite nome: ', function (done) {
-  let user = new User ({
-      name: "Angelo",
-      surname: "Corsaro",
-      email: "angelo@libero.it",
-      dateOfBirth: "20-05-2001",
-      gender: "M"
-  });
-  user.save(function (err, user) {
-      chai.request(app)
-      .get('/users/' + user.name)
-      .end(function (err, res) {
-          expect(res.status).to.equal(200);
-          expect(res.body).to.be.a('object');
-          expect(res.body.name).to.equal(user.name);
-          done();
-      });
-  });
-});
